a758b11162fec8fe5fc078fa59e744660c79fe71
Adiel Pereira <adielp16@gmail.com>
2023-10-22 20:07:20 -0300

    feat: add styles to atom components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HomePage from "../presentation/pages/home";
+import '../presentation/styles/main.scss'
 
 export default function Home() {
   return (
--- src/presentation/pages/home.tsx
+++ src/presentation/pages/home.tsx
@@ -2,11 +2,14 @@
 import React from 'react';
 import MainLayout from '../components/templates/mainLayout';
 import SearchBar from '../components/organisms/searchBar';
+import StatusLabel from '../components/atoms/statusLabel';
 
 const HomePage = () => {
   return (
     <MainLayout>
       <SearchBar onChange={() => {}} onSubmit={() => {}} value=''/>
+      <StatusLabel status='success'/>
+      <StatusLabel status='fail'/>
     </MainLayout>
   );
 };
